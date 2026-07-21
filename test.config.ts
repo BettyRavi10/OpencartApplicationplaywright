115f790e2cb95c6fd539f5e903bdf57243047388
BettyRavi10 <betty.sn@gmail.com>
2026-07-21 19:16:04 -0400

    first commit

---
+++ test.config.ts
@@ -0,0 +1,13 @@
+export class TestConfig{
+
+appUrl="http://localhost/opencart/upload/"
+
+//valid login credentials- create your own login account
+email="betty@gmail.com"
+password="Jesusbetty$77"
+
+//product details
+productName="MacBook"
+productQuantity="2"
+totalPrice="$1,204.00"
+}
