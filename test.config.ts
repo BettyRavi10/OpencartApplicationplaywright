29716c5624b6684f40bc3cf5a4ef3ca96e282849
BettyRavi10 <betty.sn@gmail.com>
2026-07-23 11:59:27 -0400

    testconfig-modified format

--- test.config.ts
+++ test.config.ts
@@ -1,15 +1,15 @@
-export class TestConfig{
+export class TestConfig {
 
-//appUrl="http://localhost/opencart/upload/"
-    appUrl="https://naveenautomationlabs.com/opencart"
+    //appUrl="http://localhost/opencart/upload/"
+    appUrl = "https://naveenautomationlabs.com/opencart"
     //appUrl="https://tutorialsninja.com/demo/"
 
-//valid login credentials- create your own login account
-email="betty@gmail.com"
-password="Jesusbetty$77"
+    //valid login credentials- create your own login account
+    email = "betty@gmail.com"
+    password = "Jesusbetty$77"
 
-//product details
-productName="MacBook"
-productQuantity="2"
-totalPrice="$1,204.00"
+    //product details
+    productName = "MacBook"
+    productQuantity = "2"
+    totalPrice = "$1,204.00"
 }
